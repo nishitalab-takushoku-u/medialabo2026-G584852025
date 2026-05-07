fe0e0ef6ca5ccdd4706269f4796c6b4f2e2a8349
G584852025 <258485@st.takushoku-u.ac.jp>
2026-05-07 17:11:04 +0900

    5/7

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,19 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+console.log(campus.address);
+
+let ns = ["D101","D102","D103","D201","D202","D203","D204","D205"];
+for (let n of ns) {
+    
+    console.log(n);
+    
+}
+
+let ms = ["機械システム工学科","電子システム工学科","情報工学科","デザイン学科"];
+for (let m of ms){
+
+	console.log(m);
+	
+}
 
